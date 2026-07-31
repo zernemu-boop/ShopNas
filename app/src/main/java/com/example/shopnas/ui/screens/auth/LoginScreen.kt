42a0277e6339c035a95575913c92c4380a8e1e5a
zernrmu-boop <zernemu@gmail.com>
2026-07-31 10:02:17 +0300

    Updated

--- app/src/main/java/com/example/shopnas/ui/screens/auth/LoginScreen.kt
+++ app/src/main/java/com/example/shopnas/ui/screens/auth/LoginScreen.kt
@@ -1,19 +1,122 @@
 package com.example.shopnas.ui.screens.auth
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Password
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import com.example.shopnas.R
+import com.example.shopnas.ui.theme.DarkSurface
+import com.example.shopnas.ui.theme.LightSecondary
+import com.example.shopnas.ui.theme.Purple80
 
 @Composable
 fun LoginScreen(navController: NavController){
 
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+
     Column(
-        modifier = Modifier.fillMaxSize()
-    ) { }
+        modifier = Modifier
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Image(
+            painter = painterResource(R.drawable.cart),
+            contentDescription = "register",
+            modifier = Modifier.size(220.dp)
+        )
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        Text(
+            text = "Welcome Back!",
+            style = MaterialTheme.typography.displaySmall,
+            fontWeight = FontWeight.ExtraBold,
+            color = Purple80,
+            textAlign = TextAlign.Center
+        )
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        OutlinedTextField(
+            value = email,
+            onValueChange ={ email= it },
+            modifier = Modifier.width(350.dp),
+            leadingIcon = { Icon(imageVector = Icons.Default.Email, contentDescription = "") },
+            label = {Text(text = "Email Address")},
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
+            colors = OutlinedTextFieldDefaults.colors(
+                unfocusedBorderColor = LightSecondary,
+                focusedBorderColor = DarkSurface,
+                unfocusedLeadingIconColor = LightSecondary,
+                focusedLeadingIconColor = DarkSurface
+            )
+        )
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        //Password
+        OutlinedTextField(
+            value = password,
+            onValueChange ={ password= it },
+            modifier = Modifier.width(350.dp),
+            leadingIcon = { Icon(imageVector = Icons.Default.Password, contentDescription = "") },
+            label = {Text(text = "Password")},
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
+            colors = OutlinedTextFieldDefaults.colors(
+                unfocusedBorderColor = LightSecondary,
+                focusedBorderColor = DarkSurface,
+                unfocusedLeadingIconColor = LightSecondary,
+                focusedLeadingIconColor = DarkSurface
+            ),
+            visualTransformation = PasswordVisualTransformation()
+        )
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        Button(
+            onClick = { },
+            colors = ButtonDefaults.buttonColors(containerColor = LightSecondary),
+            shape = RoundedCornerShape(16.dp),
+            modifier = Modifier.width(200.dp).height(56.dp)
+        ) {
+            Text(text = "Register", style = MaterialTheme.typography.titleMedium)
+        }
+
+    }
 
 }
 
